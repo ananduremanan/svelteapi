e6842ab114f9a519fe2125e7080e82ca74c233f9
Anandhu Remanan <48993261+ananduremanan@users.noreply.github.com>
2023-10-10 05:47:10 +0000

    Basic Api

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,54 @@
 <script lang="ts">
-  let counter = 0;
+  let userId: any;
+  let userData: any;
+  $: userName = userData && userData.userName;
+  $: userGender = userData && userData.userGender;
+  $: userAvatar = userData && userData.userAvatar;
+
+  const handleUserId = (event: any) => {
+    event.preventDefault();
+    userId = event.target.value;
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/getUserDetails?id=${userId}`
+      );
+      const data = await res.json();
+      userData = data;
+      console.log(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 </script>
 
-<main>
-  <div class="counter">
-    <button on:click={() => {counter += 1}}>+</button>
-      <b>{counter}</b>
-    <button on:click={() => {counter -= 1}}>-</button>
+<main class="flex flex-col justify-center items-center h-screen">
+  <div class="flex gap-1">
+    <input
+      type="text"
+      placeholder="Enter User ID"
+      class="bg-slate-200 p-2 rounded-lg"
+      on:input={handleUserId}
+    />
+    <button
+      type="submit"
+      class="bg-orange-300 p-2 rounded-lg"
+      on:click={fetchUserData}>Search</button
+    >
+  </div>
+  <div class="mt-2 flex flex-col">
+    {#if !userName}
+      <p>Search with User ID</p>
+    {:else}
+      <div class="flex gap-2 bg-yellow-300 p-2 rounded-lg w-64">
+        <img src={userAvatar} width="50" height="50" alt="user avatar" />
+        <div>
+          <h1>Name: {userName}</h1>
+          <h1>Gender: {userGender}</h1>
+        </div>
+      </div>
+    {/if}
   </div>
 </main>
-
-<style>
-  .counter{
-    background: bisque;
-    padding: 1rem;
-    border-radius: 0.8rem;
-  }
-</style>
