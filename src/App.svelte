5658ab8ddb614be9014db06ac52a010b7737fbb4
ananduremanan <ananduor@gmail.com>
2023-10-10 10:10:12 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let counter = 0;
+</script>
+
+<main>
+  <div class="counter">
+    <button on:click={() => {counter += 1}}>+</button>
+      <b>{counter}</b>
+    <button on:click={() => {counter -= 1}}>-</button>
+  </div>
+</main>
+
+<style>
+  .counter{
+    background: bisque;
+    padding: 1rem;
+    border-radius: 0.8rem;
+  }
+</style>
