cac64d39ef1b50b7fda6beaba42d5ebc644d0bb4
Anandhu Remanan <48993261+ananduremanan@users.noreply.github.com>
2023-10-11 04:20:12 +0000

    Puzzle Added

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   let userId: any;
   let userData: any;
   $: userName = userData && userData.userName;
   $: userGender = userData && userData.userGender;
   $: userAvatar = userData && userData.userAvatar;
+
+  let puzzles: any[] = [];
+  let currentPuzzle: any = { puzzle: "..." };
+  let answer: string = "";
+
+  const fetchPuzzle = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/ananduremanan/Demo/main/puzzle.json"
+      );
+      puzzles = await response.json();
+      generatePuzzle();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onMount(async () => {
+    fetchPuzzle();
+  });
+
+  function generatePuzzle() {
+    const randomIndex = Math.floor(Math.random() * puzzles.length);
+    currentPuzzle = puzzles[randomIndex];
+    answer = "";
+  }
+
+  function revealAnswer() {
+    answer = currentPuzzle.answer;
+  }
 
   const handleUserId = (event: any) => {
     event.preventDefault();
@@ -50,5 +82,25 @@
         </div>
       </div>
     {/if}
+  </div>
+  <div
+    class="flex flex-col justify-center items-center bg-slate-100 w-64 p-2 rounded-lg mt-2 gap-2"
+  >
+    <p class="text-xs">Puzzle Yourself, Guess the Movie</p>
+    <div
+      id="puzzle"
+      role="button"
+      tabindex="0"
+      on:click={revealAnswer}
+      on:keydown={revealAnswer}
+    >
+      {currentPuzzle.puzzle}
+    </div>
+    {#if answer}
+      <div id="answer" class="bg-white p-1 rounded-md text-xs">{answer}</div>
+    {/if}
+    <button on:click={fetchPuzzle} class="bg-orange-300 p-2 rounded-lg"
+      >Refresh</button
+    >
   </div>
 </main>
